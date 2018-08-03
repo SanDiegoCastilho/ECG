6f2738f4dcf8e7be11352423e62a5019a6088247
SanDiegoCastilho <sandiegocastilho@gmail.com>
2018-08-03 15:34:01 -0300

    webservice

---
+++ ecgSBBD/src/main/java/br/ufc/arida/ecg/controller/FileController.java
@@ -0,0 +1,34 @@
+package br.ufc.arida.ecg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.ufc.arida.ecg.service.FileStorageService;
+
+@RestController
+@RequestMapping(value = "/files")
+public class FileController {
+	
+	@Autowired
+	public FileStorageService fileStorageService;
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = {"multipart/form-data"})
+	@ResponseBody
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+            RedirectAttributes redirectAttributes) {
+		
+
+		fileStorageService.store(file);
+
+
+		return "deu certo";
+	}
+	
+
+}
---
+++ ecgSBBD/src/main/java/br/ufc/arida/ecg/controller/PersonController.java
@@ -0,0 +1,29 @@
+package br.ufc.arida.ecg.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufc.arida.ecg.model.Person;
+import br.ufc.arida.ecg.service.PersonService;
+
+@RestController
+@RequestMapping(value = "/persons")
+public class PersonController {
+	
+	@Autowired
+	public PersonService personService;
+
+	
+	@RequestMapping(value= "", method = RequestMethod.GET)
+	public List<Person>  persons(){
+	
+		return personService.persons();
+		
+	}
+	
+
+}
